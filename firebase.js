cd3325a113d676b4abae1b9b6d777e92bced5af2
jefta-jose <ndegwajeff4@gmail.com>
2025-01-26 23:02:30 +0300

    back

---
+++ firebase.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+const { initializeApp } = require("firebase/app");
+const { getAuth } = require("firebase/auth");
+const { getFirestore } = require("firebase/firestore");
+const { getStorage } = require("firebase/storage");
+
+require('dotenv').config();
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID ,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+const storage = getStorage();
+
+// Export the initialized instances
+module.exports = { app, auth, db, storage };
