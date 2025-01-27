3ac7afadf603eef1f912d91c7d1062c68a5bea64
jefta-jose <ndegwajeff4@gmail.com>
2025-01-27 03:04:57 +0300

    auth

--- api/reset-password.js
+++ api/reset-password.js
@@ -1,6 +1,27 @@
 import { db } from '../firebase.js';
 import { collection, query, where, getDocs, doc, deleteDoc } from 'firebase/firestore';
 import {auth} from '../firebase.js'
+const admin = require('firebase-admin');
+
+
+const serviceAccount = {
+  "type": process.env.SERVICE_ACCOUNT_TYPE,
+  "project_id": process.env.PROJECT_ID,
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+  "universe_domain": process.env.UNIVERSE_DOMAIN,
+};
+
+
+admin.initializeApp({
+credential: admin.credential.cert(serviceAccount),
+});
 
 const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true);
@@ -44,11 +65,13 @@ const handler = async (req, res) => {
       await deleteDoc(doc(db, 'verificationTokens', tokenDoc.id));
 
       // Verify the user's token with Firebase Authentication (check if it's valid)
-      const user = await auth.getUserByEmail(email);
+      const user = await admin.auth.getUserByEmail(email);
 
       if (user) {
+        console.log(`Found user: ${email}`);
+            
         // Update the password using Firebase Authentication
-        await auth.updateUser(user.uid, { password: newPassword });
+        await admin.auth().updateUser(user.uid, { password: newPassword });
 
         res.status(200).json({ message: 'Password reset successful!' });
       } else {
