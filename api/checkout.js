06d22256ba8c6127951a80a144b359eb83a69072
Jeff Ndegwa <jeff.ndegwa@griffinglobaltech.com>
2025-01-28 22:05:29 +0300

    cors

--- api/checkout.js
+++ api/checkout.js
@@ -2,7 +2,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', 'https://ecom-frontend-seven-chi.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
